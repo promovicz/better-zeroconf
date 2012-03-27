4540ace67c7ae5fbd0b11e6f3c4733a646f0199e
Ingo Albrecht <prom@berlin.ccc.de>
2012-03-27 22:26:28 +0200

    Provide more data.

--- library/src/prom/android/zeroconf/model/ZeroConfRecord.java
+++ library/src/prom/android/zeroconf/model/ZeroConfRecord.java
@@ -8,9 +8,25 @@ import android.os.Parcelable;
 
 public class ZeroConfRecord implements Parcelable {
 
+	/** System-wide unique key. */
 	public String key = "";
+	
+	/** User-visible service name */
 	public String name = "";
+	/** User-visible type name */
 	public String type = "";
+	
+	public String domain = "";
+	public String protocol = "";
+	public String application = "";
+	public String instance = "";
+	public String subtype = "";
+	public String server = "";
+	
+	public int port = 0;
+	
+	public int priority = 0;
+	public int weight = 0;
 
 	public String[] urls;
 
@@ -21,11 +37,23 @@ public class ZeroConfRecord implements Parcelable {
 	public void updateFromServiceEvent(ServiceEvent event) {
 		ServiceInfo info = event.getInfo();
 
-		this.type = event.getType();
-		this.name = event.getName();
-
 		this.key = info.getKey();
+		
+		this.name = event.getName();
+		this.type = event.getType();
+		
+		this.domain = info.getDomain();
+		this.protocol = info.getProtocol();
+		this.application = info.getApplication();
+		this.instance = info.getName();
+		this.subtype = info.getSubtype();
+		this.server = info.getServer();
 
+		this.port = info.getPort();
+		
+		this.priority = info.getPriority();
+		this.weight = info.getWeight();
+		
 		this.urls = info.getURLs().clone();
 	}
 
@@ -37,16 +65,44 @@ public class ZeroConfRecord implements Parcelable {
 
 	private ZeroConfRecord(Parcel in) {
 		key = in.readString();
+		
 		name = in.readString();
 		type = in.readString();
+		
+		domain = in.readString();
+		protocol = in.readString();
+		application = in.readString();
+		instance = in.readString();
+		subtype = in.readString();
+		server = in.readString();
+		
+		port = in.readInt();
+		
+		priority = in.readInt();
+		weight = in.readInt();
+		
 		urls = in.createStringArray();
 	}
 
 	@Override
 	public void writeToParcel(Parcel dest, int flags) {
 		dest.writeString(key);
+		
 		dest.writeString(name);
 		dest.writeString(type);
+		
+		dest.writeString(domain);
+		dest.writeString(protocol);
+		dest.writeString(application);
+		dest.writeString(instance);
+		dest.writeString(subtype);
+		dest.writeString(server);
+		
+		dest.writeInt(port);
+		
+		dest.writeInt(priority);
+		dest.writeInt(weight);
+		
 		dest.writeStringArray(urls);
 	}
 
