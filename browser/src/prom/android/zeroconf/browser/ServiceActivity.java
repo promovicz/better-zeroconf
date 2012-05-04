7f2732dc659bf0579c42306e83fa18aa28c46de6
Ingo Albrecht <prom@berlin.ccc.de>
2012-05-04 22:10:40 +0200

    Initial property UI.

--- browser/src/prom/android/zeroconf/browser/ServiceActivity.java
+++ browser/src/prom/android/zeroconf/browser/ServiceActivity.java
@@ -1,10 +1,17 @@
 package prom.android.zeroconf.browser;
 
+import java.util.List;
+
 import prom.android.zeroconf.model.ZeroConfRecord;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class ServiceActivity extends Activity {
@@ -13,12 +20,16 @@ public class ServiceActivity extends Activity {
 
 	ZeroConfRecord record;
 	
+	List<String> propertyNames;
+	ArrayAdapter<String> propertyAdapter;
+	
 	private TextView nameText;
 	private TextView typeText;
 	private TextView portText;
 	private TextView protocolText;
 	private TextView priorityText;
 	private TextView weightText;
+	private ListView propertyList;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +47,7 @@ public class ServiceActivity extends Activity {
 		protocolText = (TextView)findViewById(R.id.protocol);
 		priorityText = (TextView)findViewById(R.id.priority);
 		weightText = (TextView)findViewById(R.id.weight);
+		propertyList = (ListView)findViewById(R.id.properties);
 		
 		Log.d(TAG, "Unpacking extras bundle");
 		
@@ -65,6 +77,33 @@ public class ServiceActivity extends Activity {
 		protocolText.setText(record.protocol);		
 		priorityText.setText(Integer.toString(record.priority));
 		weightText.setText(Integer.toString(record.weight));
+		
+		propertyNames = record.getPropertyNames();
+				
+		propertyAdapter = new PropertyAdapter(
+				getApplicationContext(),
+				R.layout.service_properties,
+				R.id.name);
+		
+		propertyList.setAdapter(propertyAdapter);
+	}
+	
+	private class PropertyAdapter extends ArrayAdapter<String> {
+		PropertyAdapter(Context context, int viewLayout, int nameId) {
+			super(context, viewLayout, nameId, propertyNames);
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View v = super.getView(position, convertView, parent);
+			
+			TextView valueText = (TextView)v.findViewById(R.id.value);
+						
+			valueText.setText(record.getPropertyString(propertyNames.get(position)));
+			
+			return v;
+		}
+		
 	}
 
 }
